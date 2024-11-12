79361a8dd5ed5e83c7fc35a9b6c4cc86c123dcd3
kelly.joaquina@hotmail.com <kelly.joaquina@hotmail.com>
2024-11-12 15:30:38 +0000

    operational margin calculated in  int_orders_operational

---
+++ models/intermediate/int_orders_operational.sql
@@ -0,0 +1,13 @@
+-- Operational_margin = margin + shipping_fee - log_cost - ship_cost
+
+SELECT
+    int_orders_margin.orders_id
+    ,int_orders_margin.date_date
+    ,ROUND((SUM(margin) + SUM(shipping_fee) - SUM(logcost) - SUM(ship_cost)),2) AS operational_margin
+    ,SUM(quantity) AS quantity
+    ,SUM(revenue) AS revenue
+    ,SUM(purchase_cost) AS purchase_cost
+FROM {{ref("int_orders_margin")}}
+LEFT JOIN {{ref("stg_raw__ship")}}
+ON (int_orders_margin.orders_id=stg_raw__ship.orders_id)
+GROUP BY int_orders_margin.orders_id, int_orders_margin.date_date
