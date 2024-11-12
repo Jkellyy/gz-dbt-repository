43f9a12adc8dffaa8bbef860bc5a109769eec11b
kelly.joaquina@hotmail.com <kelly.joaquina@hotmail.com>
2024-11-12 14:44:11 +0000

    margin and purchase cost calculates

--- models/intermediate/int_sales_margin.sql
+++ models/intermediate/int_sales_margin.sql
@@ -1,6 +1,16 @@
-select revenue,
-    ,quantity
-    ,purchase_price
+ --Calcul de purchase cost en joignant sales et product dans une subquery
+ --Calcul de margin ensuite 
+ 
+
+WITH cal_purchasecost AS (
+select *,
+    (quantity*purchase_price) AS purchase_cost
 from {{ref("stg_raw__sales")}}
-JOIN {{ref("stg_raw__product")}}
-using(product_id)
+LEFT JOIN {{ref("stg_raw__product")}}
+ON (products_id=product_id)
+)
+
+SELECT *,
+    (revenue-purchase_cost) AS margin
+from cal_purchasecost
+
